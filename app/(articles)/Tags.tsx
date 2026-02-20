4df2ef55c4b1cac562aecbeaa7b9cd98418dd896
CalvoJohnPaul <looselytiedshoelace@gmail.com>
2026-02-20 22:14:28 +0800

    add 404

--- app/(articles)/Tags.tsx
+++ app/(articles)/Tags.tsx
@@ -5,12 +5,13 @@ import {useTagsQuery} from '~/hooks/useTagsQuery';
 
 export function Tags() {
   const query = useTagsQuery();
+  const tags = query.data ?? [];
 
   return (
     <section className="order-0 w-full shrink-0 rounded bg-gray-100 p-4 lg:order-1 lg:w-64">
       <h2 className="tracking-wide">Popular Tags</h2>
       <ul className="mt-2.5 flex flex-wrap gap-1">
-        {query.data?.map((tag) => (
+        {tags.map((tag) => (
           <li key={tag}>
             <Link
               href={`/${tag}`}
---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="flex min-h-[80vh] items-center justify-center px-4">
+      <h1 className="font-bold font-heading text-7xl lg:text-8xl text-gray-200/75">404</h1>
+    </main>
+  );
+}
